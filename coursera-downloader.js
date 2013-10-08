5d8c5184c964696afe5a2f6d293eb74e33526afa
Shashidhar <shashidhar.dakuri@gmail.com>
2013-10-07 23:03:37 -0700

    Bugfix - Downloaded files were of 0 bytes. This fix was borrowed from https://github.com/n1k0/casperjs/issues/73

--- coursera-downloader.js
+++ coursera-downloader.js
@@ -7,6 +7,7 @@ var casper = require('casper').create({
     pageSettings: {
         loadImages: false,         // The WebPage instance used by Casper will
         loadPlugins: false,         // use these settings
+        webSecurityEnabled: false,
         userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4'
     }
 });
